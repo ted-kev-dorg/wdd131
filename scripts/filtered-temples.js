e9bc530a85eae10884e0ad15cac6be999be1edd6
ted-kev-dorg <kevin.tetede@gmail.com>
2025-07-27 02:13:08 -0400

    week 04 - Picture Album Enhancement

---
+++ scripts/filtered-temples.js
@@ -0,0 +1,221 @@
+const temples = [
+  {
+    templeName: "Aba Nigeria",
+    location: "Aba, Nigeria",
+    dedicated: "2005, August, 7",
+    area: 11500,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/aba-nigeria/400x250/aba-nigeria-temple-lds-273999-wallpaper.jpg"
+  },
+  {
+    templeName: "Manti Utah",
+    location: "Manti, Utah, United States",
+    dedicated: "1888, May, 21",
+    area: 74792,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/manti-utah/400x250/manti-temple-768192-wallpaper.jpg"
+  },
+  {
+    templeName: "Payson Utah",
+    location: "Payson, Utah, United States",
+    dedicated: "2015, June, 7",
+    area: 96630,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/payson-utah/400x225/payson-utah-temple-exterior-1416671-wallpaper.jpg"
+  },
+  {
+    templeName: "Yigo Guam",
+    location: "Yigo, Guam",
+    dedicated: "2020, May, 2",
+    area: 6861,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/yigo-guam/400x250/yigo_guam_temple_2.jpg"
+  },
+  {
+    templeName: "Washington D.C.",
+    location: "Kensington, Maryland, United States",
+    dedicated: "1974, November, 19",
+    area: 156558,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/washington-dc/400x250/washington_dc_temple-exterior-2.jpeg"
+  },
+  {
+    templeName: "Lima Perú",
+    location: "Lima, Perú",
+    dedicated: "1986, January, 10",
+    area: 9600,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/lima-peru/400x250/lima-peru-temple-evening-1075606-wallpaper.jpg"
+  },
+  {
+    templeName: "Mexico City Mexico",
+    location: "Mexico City, Mexico",
+    dedicated: "1983, December, 2",
+    area: 116642,
+    imageUrl:
+    "https://content.churchofjesuschrist.org/templesldsorg/bc/Temples/photo-galleries/mexico-city-mexico/400x250/mexico-city-temple-exterior-1518361-wallpaper.jpg"
+  },
+  // Add more temple objects here...
+  {
+    templeName: "Accra Ghana",
+    location: "Accra, Ghana",
+    dedicated: "2004, January, 11",
+    area: 17500,
+    imageUrl:
+    "https://churchofjesuschristtemples.org/assets/img/temples/accra-ghana-temple/accra-ghana-temple-13760-main.jpg"
+  },
+  {
+    templeName: "Abidjan Ivory Coast",
+    location: "Abidjan, Ivory Coast",
+    dedicated: "2025, May, 25",
+    area: 17362,
+    imageUrl:
+    "https://churchofjesuschristtemples.org/assets/img/temples/abidjan-ivory-coast-temple/abidjan-ivory-coast-temple-58993-main.jpg"
+  },
+  {
+    templeName: "Durban South Africa",
+    location: "Durban, South Africa",
+    dedicated: "2020, February, 16",
+    area: 19860,
+    imageUrl:
+    "https://churchofjesuschristtemples.org/assets/img/temples/durban-south-africa-temple/durban-south-africa-temple-7936-main.jpg"
+  },
+  {
+    templeName: "Johannesburg South Africa",
+    location: "Johannesburg, South Africa",
+    dedicated: "1985, August, 25",
+    area: 19184,
+    imageUrl:
+    "https://churchofjesuschristtemples.org/assets/img/temples/johannesburg-south-africa-temple/johannesburg-south-africa-temple-22475-main.jpg"
+  },
+  {
+    templeName: "Aba Nigeria",
+    location: "Aba, Nigeria",
+    dedicated: "2005, August, 7",
+    area: 11500,
+    imageUrl:
+    "https://churchofjesuschristtemples.org/assets/img/temples/aba-nigeria-temple/aba-nigeria-temple-5087-main.jpg"
+  },
+  {
+    templeName: "Nairobi Kenya",
+    location: "Nairobi, Kenya",
+    dedicated: "2025, May, 18",
+    area: 19870,
+    imageUrl:
+    "https://churchofjesuschristtemples.org/assets/img/temples/nairobi-kenya-temple/nairobi-kenya-temple-60488-main.jpg"
+  },
+];
+
+
+function cardGenerator (temple) {
+	return `<div>
+				<h1>${temple.templeName}</h1>
+				<p><span>Location:</span> ${temple.location}</p>
+				<p><span>Dedicated:</span> ${temple.dedicated}</p>
+				<p><span>Size:</span> ${temple.area} sq ft</p>
+				<figure>
+					<img src="${temple.imageUrl}" alt="${temple.templeName} Temple" loading="lazy">
+				</figure>
+			</div>`;
+}
+
+function renderCard (temples){
+	const html = temples.map(cardGenerator);
+	document.querySelector("main").innerHTML = html.join("");
+}
+
+renderCard(temples);
+
+const all = document.getElementById("all");
+const old = document.getElementById("old");
+const recent = document.getElementById("new");
+const large = document.getElementById("large");
+const small = document.getElementById("small");
+
+all.addEventListener("click", () =>{
+	renderCard(temples);
+
+});
+
+old.addEventListener("click", () => {
+  renderCard(temples.filter(temple => {
+    const year = parseInt(temple.dedicated.split(",")[0]);
+    return year < 1900;
+  }));
+});
+
+recent.addEventListener("click", () => {
+	renderCard(temples.filter(temple =>{
+		const year = parseInt(temple.dedicated.split(",")[0]);
+		return year > 2000;
+	}));
+});
+
+large.addEventListener("click", () => {
+	renderCard (temples.filter(temple =>{
+		const area = temple.area;
+		return area > 90000;
+	}));
+
+});
+
+small.addEventListener("click", () => {
+	renderCard (temples.filter(temple =>{
+		const area = temple.area;
+		return area < 10000;
+	}));
+
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// Set current year
+let year = new Date().getFullYear();
+document.getElementById("currentyear").textContent = year;
+
+// Set last modified date
+let lastModified = document.lastModified;   
+document.getElementById('lastModified').textContent = `Last Modified: ${lastModified}`;
+
+
+const hamButton = document.querySelector("#menu");
+const navigation = document.querySelector(".navigation");
+const title = document.querySelector("#title"); 
+
+hamButton.addEventListener('click', () => {
+	navigation.classList.toggle('open');
+	hamButton.classList.toggle('open');
+    title.classList.toggle("hidden");
+});
